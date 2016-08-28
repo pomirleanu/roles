8784d25c992723237b019fe42082519e5337af2a
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-08-28 13:06:26 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/migrations/2015_01_26_115212_create_permissions_table.php
+++ src/migrations/2015_01_26_115212_create_permissions_table.php
@@ -12,7 +12,7 @@ class CreatePermissionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('permissions', function (Blueprint $table) {
+        Schema::create('permissions', function(Blueprint $table) {
             $table->increments('id')->unsigned();
             $table->string('name');
             $table->string('slug')->unique();
--- src/migrations/2015_01_26_115523_create_permission_role_table.php
+++ src/migrations/2015_01_26_115523_create_permission_role_table.php
@@ -12,7 +12,7 @@ class CreatePermissionRoleTable extends Migration
      */
     public function up()
     {
-        Schema::create('permission_role', function (Blueprint $table) {
+        Schema::create('permission_role', function(Blueprint $table) {
             $table->increments('id')->unsigned();
             $table->integer('permission_id')->unsigned()->index();
             $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
